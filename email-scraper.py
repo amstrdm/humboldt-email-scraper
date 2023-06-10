1f2f8a18aadbeab79f0e93b4a670264c01cbfad7
amstrdm <132276399+amstrdm@users.noreply.github.com>
2023-06-10 14:31:44 +0200

    added the python script

---
+++ email-scraper.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup as bs
+import requests
+import re
+
+links = ["https://humboldt-schule.de/kollegium/", "https://humboldt-schule.de/kollegium/page/2/", "https://humboldt-schule.de/kollegium/page/3/", "https://humboldt-schule.de/kollegium/page/4", "https://humboldt-schule.de/kollegium/page/5", "https://humboldt-schule.de/kollegium/page/6", "https://humboldt-schule.de/kollegium/page/7", ]
+
+for i in links:
+    page = requests.get(i)
+    # <Response [200]>
+    soup = bs(page.content)
+    email_raw = soup.find_all(class_='mail')
+    string_email_raw = str(email_raw)
+
+    match = re.findall('\S+@\S+' , string_email_raw)   
+    
+    
+    with open('D:\coding\Python\emails.txt', mode='a', encoding='utf-8') as myfile:
+            myfile.write('\n'.join(match))
+            myfile.write('\n')
